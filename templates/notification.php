21920be51570da3284201370e9b63fc4080cdb27
Biscuit <gtaw@booskit.dev>
2025-08-29 13:26:22 +0200

    Update notification.php

--- templates/notification.php
+++ templates/notification.php
@@ -1,76 +1,12 @@
 <?php
-if ($g->findCookie('notificationVersion') != $g->getSettings('site-version')) {
-?>
-<div id="notification">
-	<div class="container">
-		<i class="mr-1 fas fa-fw fa-plug"></i>Update
-		<a class="mx-1" href="/changelogs#<?= $g->getSettings('site-version') ?>">
-			<span class="badge badge-dark"><?= $g->getSettings('site-version') ?></span>
-		</a>
-		is now out!
-		<a class="ml-3" id="notification-dissmiss"><span class="badge badge-trans p-2"><i class="fas fa-fw fa-times"></i></span></a>
-	</div>
-</div>
-<?php
-}
-?>
-
 <?php
 if ($g->getSettings('special-notification') && ($g->findCookie('specialNotification') != $g->getSettings('special-notification'))) {
 ?>
 <div id="special-notification">
 	<div class="container">
 		<i class="mr-1 fas fa-fw fa-bullhorn"></i><?= $g->getSettings('special-notification-msg') ?>
-		<a class="ml-3" id="special-notification-dissmiss"><span class="badge badge-trans p-2"><i class="fas fa-fw fa-times"></i></span></a>
 	</div>
 </div>
 <?php
 }
 ?>
-<script type="text/javascript">
-	$(document).ready(function() {
-		// Hide notification
-		$('body').on('click', '#notification-dissmiss', function (e) {
-
-			e.preventDefault();
-
-			$.ajax({
-				url: '/controllers/form-processor.php',
-				type: 'POST',
-				data: {
-					getType: 'setNotificationVersion'
-				},
-				success: function(response) {
-					$('#notification').css('transform', 'translateY(-48.5px)');
-					$('#notification').css('opacity', '0');
-					$('#breadcrumb').css('transform', 'translateY(-48.5px)');
-					$('#footer').css('transform', 'translateY(-48.5px)');
-				},
-			});
-
-		});
-
-		// Hide special notification
-		$('body').on('click', '#special-notification-dissmiss', function (e) {
-
-			e.preventDefault();
-
-			$.ajax({
-				url: '/controllers/form-processor.php',
-				type: 'POST',
-				data: {
-					getType: 'hideSpecialNotification'
-				},
-				success: function(response) {
-					$('#special-notification').css('transform', 'translateY(-48.5px)');
-					$('#special-notification').css('opacity', '0');
-					$('#breadcrumb').css('transform', 'translateY(-48.5px)');
-					$('#footer').css('transform', 'translateY(-48.5px)');
-				},
-			});
-
-		});
-
-
-	});
-</script>
