f794a9e8685072e8b795a7993f2c69c3059fb209
Biscuit <gtaw@booskit.dev>
2025-08-29 13:29:24 +0200

    Update notification.php

--- templates/notification.php
+++ templates/notification.php
@@ -2,7 +2,7 @@
 <?php
 if ($g->getSettings('special-notification') && ($g->findCookie('specialNotification') != $g->getSettings('special-notification'))) {
 ?>
-<div id="special-notification">
+<div id="notification">
 	<div class="container">
 		<i class="mr-1 fas fa-fw fa-bullhorn"></i><?= $g->getSettings('special-notification-msg') ?>
 	</div>
